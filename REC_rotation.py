94f9de9474038031f8793b0434196305a7caa166
jprieto <julia.prieto-santero@etu.univ-lyon1.fr>
2019-07-04 22:27:22 +0200

    Success in correction of ccmap construction

--- REC_rotation.py
+++ REC_rotation.py
@@ -1,40 +1,77 @@
 #! /usr/bin/env python3
-import sys, os, math, numpy as np
+import sys, os, numpy as np
+from math import cos, sin, acos, asin
 import pyproteinsExt.structure.coordinates as PDB
 sys.path.append("DockingPP")
 from dockingPP import zParse
+from pyproteinsExt.structure.operations import ContactMap
 
-parser = PDB.Parser()
-pdbObj = parser.load(file="data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_r_u_2.pdb")
-
-phi=2
-theta=5
-psi=3
 
 # print(pdbObj.atomDictorize['x'])
+def where(O,astr):
+    count=0
+    for i in O:
+        if astr in i:
+            count+=1
+    return count
+
+def rotate(x,y,z,psi,theta,phi) :
+    r11 = cos(psi)*cos(phi)  -  sin(psi)*cos(theta)*sin(phi)
+    r21 = sin(psi)*cos(phi)  +  cos(psi)*cos(theta)*sin(phi)
+    r31 = sin(theta)*sin(phi)
+
+    r12 = -cos(psi)*sin(phi)  -  sin(psi)*cos(theta)*cos(phi)
+    r22 = -sin(psi)*sin(phi)  +  cos(psi)*cos(theta)*cos(phi)
+    r32 = sin(theta)*cos(phi)
+
+    r13 = sin(psi)*sin(theta)
+    r23 = -cos(psi)*sin(theta)
+    r33 = cos(theta)
+
+    new_x= r11 * x + r12 * y + r13 * z
+    new_y= r21 * x + r22 * y + r23 * z
+    new_z= r31 * x + r32 * y + r33 * z
+
+    return (new_x, new_y, new_z)
+
 
 def trans_matrix(psi,theta,phi):
     """Construction de la matrice de transition a partir des angles d'euler"""
-    cp=math.cos(psi)
-    sp=math.sin(psi)
-    ct=math.cos(theta)
-    st=math.sin(theta)
-    cf=math.cos(phi)
-    sf=math.sin(phi)
+    r11 = cos(psi)*cos(phi)  -  sin(psi)*cos(theta)*sin(phi)
+    r21 = sin(psi)*cos(phi)  +  cos(psi)*cos(theta)*sin(phi)
+    r31 = sin(theta)*sin(phi)
 
-    tr_matrix = np.matrix([[cp*cf-sp*ct*sf,sp*cf+cp*ct*sf,st*sf ],[-cp*sf-sp*ct*sf,-sp*sf+cp*ct*cf,st*cf],[sp*st,-cp*st,ct]])
-    tr_matrix=tr_matrix.T
+    r12 = -cos(psi)*sin(phi)  -  sin(psi)*cos(theta)*cos(phi)
+    r22 = -sin(psi)*sin(phi)  +  cos(psi)*cos(theta)*cos(phi)
+    r32 = sin(theta)*cos(phi)
+
+    r13 = sin(psi)*sin(theta)
+    r23 = -cos(psi)*sin(theta)
+    r33 = cos(theta)
+
+    tr_matrix = np.matrix([[r11, r12, r13 ],[r21, r22, r23],[r31, r32, r33]])
+
     return tr_matrix
 
+def euleurFromMatrix(matrix):
+    t=-acos(matrix[2,2])
+    p=asin(matrix[0,2]/sin(t))
+    f=asin(matrix[2,0]/sin(t))
+    print(cos(t))
+    print(sin(t)*sin(f))
+    print(sin(t)*sin(p))
+
+    return (p,t,f)
+
+def vertical_matrix(P):
+    ver_coordinates=np.matrix([[i] for i in P])
+    return ver_coordinates
 
 
-def coord_matrix(P):
-    """O,P tuples with 3 values : the x, y and z coordinates of a rotation origin - O
-    and of a rotating point or atom - P
-    Returns a numpy array with dimensions (1,3) """
+def translate(x,y,z,t1,t2,t3):
 
-    rel_coordinates=np.matrix([[P[i]] for i in range(3)])
-    return rel_coordinates
+    return (x + t1, y + t2, z + t3)
+
 
 def transpose(A,B):
     """A,B two numpy matrixes A a transition matrix, 'matrice de passage'
@@ -59,11 +96,14 @@ def recenter(O,P):
 #     else :
 #         return False
 
-DD=zParse("data/decoys_bm4_zd3.0.2_6deg_fixed/results/1HIA.zd3.0.2.fg.fixed.out")
+DD=zParse("../data/decoys_bm4_zd3.0.2_6deg_fixed/results/1HIA.zd3.0.2.fg.fixed.out")
 # print(DD.baryREC)
 # print(DD.eulerREC)
-DD.setReceptor("data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_r_u_2.pdb")
-DD.setLigand("data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_l_u_2.pdb")
+# DD.setReceptor("data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_r_u_2.pdb")
+# DD.setLigand("data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_l_u_2.pdb")
+DD.setReceptor("../data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_r_u_2.pdb")
+DD.setLigand("../data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_l_u_2.pdb")
+
 
 # print(DD.pdbObjLigand.atomDictorize['x'])
 
@@ -73,22 +113,83 @@ DD.setLigand("data/decoys_bm4_zd3.0.2_6deg_fixed/input_pdbs/1HIA_l_u_2.pdb")
     # sys.stdout.write(str(i))
     # sys.stdout.flush()
     # atom=DD.pdbObjReceptor.model[0][i]
-for atom in DD.pdbObjReceptor.model[0]:
-    initial_coords=(atom.x, atom.y, atom.z)
-    a=trans_matrix(*DD.eulerREC)
-    b=coord_matrix(initial_coords)
-    # print(b)
-    coords=DD.baryREC,transpose(a,b)
-    atom.x=coords[0,0]
-    atom.y=coords[1,0]
-    atom.z=coords[2,0]
+# print(DD.pdbObjLigand)
+expected="ATOM      1  N   THR     5      13.140  23.779  -6.666"
 
+# print(f"baryLIG : {DD.baryLIG} \n random : {DD.eulerREC} \n pose rotation : {DD.pList[0].euler}\n pose translation : {DD.pList[0].translate} \n baryREC : {DD.baryREC}")
+
+# This works !!!!
+# for atom in DD.pdbObjLigand.model[0]:
+#     if atom.serial==1 :
+#         initial_coords=(atom.x, atom.y, atom.z)
+#         print("\nInitial state = " + str(initial_coords))
+#         neg_bary_lig=[-1*i for i in DD.baryLIG]
+#
+#         coords=translate(*initial_coords, *neg_bary_lig)
+#         print("Translation to origin = " + str(coords))
+#         #Apply Initial Random rotation
+#         coords2=rotate(*coords,*DD.eulerREC)
+#         #Apply pose Rotation
+#         coords3=rotate(*coords2,*DD.pList[0].euler)
+#         print("Rotated coordinates = " + str(coords3))
+#
+#         # trans=[i for i in DD.pList[0].translate]
+#         coords4=translate(*coords3, *DD.baryREC)
+#         coords5=translate(*coords4, *DD.pList[0].translate)
+#         # print(coords4)
+#
+#
+#         print("Translation to receptor then ligand = " + str(coords5))
+#         # print(atom.chainID)
+#         # if atom.resSeq==1 :
+#             # print("x = " + str(atom.x))
+#         atom.x=coords5[0]
+#         atom.y=coords5[1]
+#         atom.z=coords5[2]
+#         if str(atom)==expected :
+#             print('Hurray')
+#
+#         break
+
+
+a=trans_matrix(*DD.eulerREC)
+b=trans_matrix(*DD.pList[0].euler)
+E1=rotate(*DD.baryLIG, *DD.eulerREC)
+E2=rotate(*E1, *DD.pList[0].euler)
+
+double=b.dot(a)
+
+new_euler=euleurFromMatrix(double)
+
+DD.pList[0].euler=new_euler
+print(double.dot(vertical_matrix(DD.baryLIG)))
+print(E2)
+print(double)
+print(trans_matrix(*new_euler))
+
+print(trans_matrix(*new_euler).dot(vertical_matrix(DD.baryLIG)))
+DD.baryLIG=tuple([-i for i in DD.baryLIG])
+DD.baryREC=tuple([-i for i in DD.baryREC])
+# print(DD.baryREC)
+# print(str(DD.pdbObjLigand)[:54])
 
 DD.pList[0].ccmap(dist=5)
 C=DD.pList[0].resMapList
+print(C)
+print(where(C,'Rec'))
+print(where(C,'Lig'))
+#
+# C=ContactMap(DD.pdbObjLigand,DD.pdbObjReceptor)
+#
+# dist=C.Q(d=5)
+# for x,y in C :
+#     print(x)
+    # print(C[(x,y)])
+# print(dist)
+
 # print(C)
 # resS=DD.resStats
-print(C)
+
 # print(C['data'])
 
 # print(DD.pdbObjReceptor)
